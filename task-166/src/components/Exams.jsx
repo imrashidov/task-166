f1003c173280cafa4578e0dd8d697bfcb1020e97
imrashidov <farikrashidov@gmail.com>
2024-02-16 00:50:48 +0400

    Add: Active buttons on filter section

--- task-166/src/components/Exams.jsx
+++ task-166/src/components/Exams.jsx
@@ -1,15 +1,19 @@
+import Sidebar from "./Sidebar";
 import Exam from "./Exam";
 import { examsData } from "../examData.js";
 
 export default function Exams() {
   return (
-    <div className="mb-28 font-medium flex flex-col items-center ">
+    <div className="mb-28 font-medium flex flex-col items-center  ">
       <p>Ən çox işlənənlər</p>
       <h3 className="font-bold text-4xl my-5 12">İmtahanlar</h3>
-      <div className="grid grid-cols-2 gap-5">
-        {examsData.map((exam) => (
-          <Exam key={exam.id} {...exam} /> // Pass exam data as props
-        ))}
+      <div className="flex justify-evenly w-full gap-10 px-36">
+        <Sidebar />
+        <div className="grid grid-cols-2 gap-5">
+          {examsData.map((exam) => (
+            <Exam key={exam.id} {...exam} /> // Pass exam data as props
+          ))}
+        </div>
       </div>
     </div>
   );
--- task-166/src/components/Sidebar.jsx
+++ task-166/src/components/Sidebar.jsx
@@ -1,3 +1,70 @@
+import { useState } from "react";
 export default function Sidebar() {
-  return <div></div>;
+  const [activeCategory, setActiveCategory] = useState(null);
+  const [activeLabel, setActiveLabel] = useState(null);
+
+  const handleCategoryClick = (category) => {
+    setActiveCategory(category); // Update active item state
+  };
+  const handleLabelClick = (item) => {
+    setActiveLabel(item); // Update active item state
+  };
+
+  const filterCategories = [
+    "İbtidai sinif",
+    "Buraxılış",
+    "Blok",
+    "Təkmilləşdirmə",
+    "Liseylərə",
+    "Magistratura",
+    "Olimpiada",
+    "Fənn imtahanı",
+  ];
+  const filterLabels = ["2-ci sinif", "3-cü sinif", "4-cü sinif"];
+
+  return (
+    <div className="w-80 p-7 ">
+      <span className="font-bold text-xl">Filter</span>
+      <div className="mt-7">
+        <span className="text-sm text-gray-400">Kategoriyalar</span>
+        <ul className="mb-10">
+          {filterCategories.map((category, index) => (
+            <li
+              key={index}
+              className={`flex flex-col my-3 cursor-pointer rounded-xl ${
+                activeCategory === category ? "active" : ""
+              }`}
+            >
+              <button
+                className="text-left p-3"
+                onClick={() => handleCategoryClick(category)}
+              >
+                {category}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="mt-7">
+        <span className="text-sm text-gray-400">Etiketlər</span>
+        <ul className="mb-10">
+          {filterLabels.map((label, index) => (
+            <li
+              key={index}
+              className={`flex flex-col  my-3 cursor-pointer rounded-xl ${
+                activeLabel === label ? "active" : ""
+              }`}
+            >
+              <button
+                className="text-left p-3"
+                onClick={() => handleLabelClick(label)}
+              >
+                {label}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
