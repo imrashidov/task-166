a1565c7c15ab4f807d7d54aae6a1ac9715438512
imrashidov <farikrashidov@gmail.com>
2024-02-16 17:44:12 +0400

    Fix: Styling Categories section for mobile

--- task-166/src/components/Categories.jsx
+++ task-166/src/components/Categories.jsx
@@ -13,14 +13,16 @@ export default function Categories() {
     <div className="flex flex-col items-center font-medium gap-5 mb-28 ">
       <p>İmtahan</p>
       <h3 className="font-bold text-4xl mb-10">Kategoriya seç</h3>
-      <div className="grid grid-cols-4 gap-5 text-center font-semibold  ">
+      <div className="grid grid-cols-4 max-md:grid-cols-2 gap-5 text-center font-semibold  ">
         {categoryLinks.map((category) => (
           <a
             href={category.link}
             key={category.name}
             className="shadow-lg bg-gray-50 rounded-xl"
           >
-            <p className="p-10 hover:text-gray-600">{category.name}</p>
+            <p className="p-10 max-md:p-7 hover:text-gray-600">
+              {category.name}
+            </p>
           </a>
         ))}
       </div>
